77a7bac0040e1a6cd03159fce5a052e4441c4cea
wangguoyan <2282940593@qq.com>
2020-06-14 01:15:33 +0800

    - 主项目地址通过json文件配置 - 修改electron引用为全局 - 隐藏菜单

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,50 @@
-const { app, BrowserWindow } = require('electron');
+const fs = require('fs');
+const path = require('path');
+const { app, Menu, BrowserWindow } = require('electron');
+// const cheerio = require('cheerio');
 
-function createWindow() {
+// 读取配置文件
+const CONFIG = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../appconfig.json'), 'utf-8'));
+
+
+/**
+ * @method main
+ * @description 应用主函数
+ */
+function main() {
+    Menu.setApplicationMenu(null);
+
+    // 使用 BrowserWindow创建窗口
     let win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1360,
+        height: 760,
+        title: 'IBatchPharma',
+        
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: false
         }
     });
 
-    win.loadURL('http://migao.pub');
+    win.webContents.executeJavaScript(`
+        console.log('注册成功！')
+    `);
+
+    win.loadURL(CONFIG.indexUrl);
 };
 
-app.whenReady().then(createWindow);
+// app就绪时运行主函数
+app.whenReady().then(main);
 
+// 当关闭所有程序界面时，退出应用
 app.on('window-all-closed', () => {
     if(process.platform !== 'darwin') {
         app.quit();
     }
 });
 
+// 应用开启
 app.on('activate', () => {
     if(BrowserWindow.getAllWindows().length === 0){
-        createWindow();
+        main();
     }
 });
