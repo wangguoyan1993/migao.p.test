ff49d6f110fcffa434f87eb56de0ab94b4b34c91
wangguoyan <2282940593@qq.com>
2020-06-14 22:56:34 +0800

    - 编译babel增加minify配置 - 编译前增加删除功能

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,32 @@
-const gulp = require('gulp');
+const fs = require('fs');
 const path = require('path');
+const gulp = require('gulp');
 const babel = require('gulp-babel');
+const del = require('del');
+
+const BABEL_CONFIG = {
+    presets: [
+        '@babel/env',
+        '@babel/preset-typescript',
+        'minify'
+    ]
+}
 
 // 定义项目主目录
 const ROOT_PATH = path.resolve(__dirname);
 
+// 删除旧的编译结果
+gulp.task('del_old_dist', gulp.series((callback) => {
+    let old_dist = path.resolve(__dirname, './dist/');
+    // if(fs.existsSync(old_dist)) fs.rmdirSync(old_dist, {recursive: true});
+    del.sync(old_dist);
+    callback();
+}));
 
-gulp.task('build', gulp.series((callback) => {
+// 主编译命令
+gulp.task('build', gulp.series(['del_old_dist'], (callback) => {
     gulp.src(`${ROOT_PATH}/src/main.ts`)
-        .pipe(babel())
+        .pipe(babel(BABEL_CONFIG))
         .pipe(gulp.dest(`${ROOT_PATH}/dist/`));
     callback();
 }));
